302c491b0e470c9794abcae933e0c6e8d713019b
hirotoe0112 <hirotoe0112@gmail.com>
2022-01-31 14:30:26 +0900

    jQueryを全てVueに変更

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,17 +1,56 @@
+var socket = io.connect();
+var chatApp = new Chat(socket);
+
 //バインドするデータ
 let vueObj = {
   room: "",
-  sendMessage:""
+  sendMessage:"",
+  messages: [],
+  rooms: []
 };
 
-//HTMLタグをエスケープして表示
-function divEscapedContentElement(message){
-  return $('<div></div>').text(message);
-}
+let app = new Vue({
+  el:'#content',
+  data:vueObj,
+  methods:{
+    send:function(event){
+      processUserInput(chatApp, socket);
+      this.focus();
+      this.scroll();
+    },
+    enter:function(event){
+      if(event.keyCode == 13){
+        processUserInput(chatApp, socket);
+        this.focus();
+      }
+    },
+    changeroom:function(event){
+      chatApp.processCommand('/join ' + event.target.innerText);
+      this.focus();
+    },
+    focus:function(){
+      this.$refs.defaultFocus.focus();
+    }
+  },
+  mounted:function(){
+    this.$nextTick(function(){
+      this.$refs.defaultFocus.focus();
+    })
+  },
+  watch:{
+    //メッセージが追加されるのを監視して下までスクロールする
+    messages:function(newMessages, oldMessages){
+      this.$nextTick(function(){
+        this.$refs.messageArea.scrollTop = this.$refs.messageArea.scrollHeight;
+      });
+    }
+  }
+});
 
-//HTMLタグをタグとして表示
-function divSystemContentElement(message){
-  return $('<div></div>').html('<i>' + message + '</i>');
+//メッセージ追加
+function addMessage(message, isSystem, isSelf){
+  var newdata = {value: message, isSystem: isSystem, isSelf: isSelf}
+  vueObj.messages.push(newdata);
 }
 
 //ユーザ入力の処理
@@ -23,86 +62,50 @@ function processUserInput(chatApp, socket){
     //コマンドの場合はコマンドを処理して処理結果を画面に表示
     systemMessage = chatApp.processCommand(message);
     if(systemMessage){
-      $('#messages').append(divSystemContentElement(systemMessage));
+      addMessage(systemMessage, true, false);
     }
   } else {
     //コマンド以外の場合はチャットメッセージとして送信
     chatApp.sendMessage(vueObj.room, message);
-    $('#messages').append(divEscapedContentElement(message));
-    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+    addMessage(message, false, true);
   }
   vueObj.sendMessage = "";
 }
 
-var socket = io.connect();
-
-$(document).ready(function(){
-  var chatApp = new Chat(socket);
-
-  let app = new Vue({
-    el:'#content',
-    data:vueObj,
-    methods:{
-      send:function(event){
-        processUserInput(chatApp, socket);
-        this.focus();
-      },
-      enter:function(event){
-        if(event.keyCode == 13){
-          processUserInput(chatApp, socket);
-          this.focus();
-        }
-      },
-      focus:function(){
-        this.$refs.defaultFocus.focus();
-      }
-    },
-    mounted:function(){
-        this.$refs.defaultFocus.focus();
-    }
-  });
-
-  //名前変更要求の結果を表示
-  socket.on('nameResult', function(result){
-    var message;
-    if(result.success){
-      message = 'You are now known as ' + result.name + '.';
-    } else {
-      message = result.message;
-    }
-    $('#messages').append(divSystemContentElement(message));
-  });
-
-  //ルーム変更の結果を表示
-  socket.on('joinResult', function(result){
-    vueObj.room = result.room;
-    $('#messages').append(divSystemContentElement('Room changed.'));
-  });
-
-  //受信したメッセージを表示
-  socket.on('message', function(message){
-    var newElement = $('<div></div>').text(message.text);
-    $('#messages').append(newElement);
-  });
-
-  //利用できるルームのリストを表示
-  socket.on('rooms', function(rooms){
-    $('#room-list').empty();
-    rooms.forEach(room => {
-      if(room != ''){
-        $('#room-list').append(divEscapedContentElement(room));
-      }
-    });
-
-    //ルームをクリックしたら移動できるようにする
-    $('#room-list div').click(function(){
-      chatApp.processCommand('/join ' + $(this).text());
-      $('#send-message').focus();
-    });
-  });
-
-  //利用できるルームを定期的に問い合わせる
-  setInterval(function(){
-    socket.emit('rooms');
-  }, 1000);
+//名前変更要求の結果を表示
+socket.on('nameResult', function(result){
+  var message;
+  if(result.success){
+    message = 'You are now known as ' + result.name + '.';
+  } else {
+    message = result.message;
+  }
+  addMessage(message, true, false);
 });
+
+//ルーム変更の結果を表示
+socket.on('joinResult', function(result){
+  vueObj.room = result.room;
+  addMessage('Room changed', true, false);
+});
+
+//受信したメッセージを表示
+socket.on('message', function(message){
+  addMessage(message.text, false, false);
+});
+
+//利用できるルームのリストを表示
+socket.on('rooms', function(rooms){
+  vueObj.rooms = [];
+  rooms.forEach(room => {
+    if(room != ''){
+      var newdata = {name: room};
+      vueObj.rooms.push(newdata);
+    }
+  });
+});
+
+//利用できるルームを定期的に問い合わせる
+setInterval(function(){
+  socket.emit('rooms');
+}, 1000);
